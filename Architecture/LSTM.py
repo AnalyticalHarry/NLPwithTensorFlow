0528fa1de05447d3454bca71acf85686daaa989d
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-02-10 01:02:17 +0000

    Update LSTM.py

--- Architecture/LSTM.py
+++ Architecture/LSTM.py
@@ -1,11 +1,17 @@
+import warnings
+import tensorflow as tf
+import matplotlib.pyplot as plt
+%matplotlib inline
+warnings.filterwarnings('ignore')
+
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional, Dropout, Attention
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 from tensorflow.keras.layers import GlobalAveragePooling1D
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
 
 # tokenize text data
 tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
